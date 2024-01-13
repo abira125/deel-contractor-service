651ebfaf6e013ddcdff82ca8d8d6ad2ef8b8ad2b
abira125 <abhinavravi125@gmail.com>
2024-01-13 19:03:42 +0000

    Add support for unpaid jobs endpoint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {Op} = require('sequelize');
 const {sequelize} = require('./model');
 const {getProfile} = require('./middleware/getProfile');
 const app = express();
@@ -57,6 +58,46 @@ app.get('/contracts/', getProfile, async (req, res) => {
     console.error(error);
     return sendError(serverError(), res);
   }
+});
+
+/**
+ * Gets unpaid jobs for a user
+ */
+app.get('/jobs/unpaid', getProfile, async (req, res) => {
+  const {Job} = req.app.get('models');
+
+  try {
+    // Get all active contracts
+    const contracts = await getActiveContracts(req.profile, req),
+      contractIds = contracts.map((contract) => {
+        return contract.id;
+      });
+
+    if (contractIds.length === 0) {
+      return res.json({unpaidJobs: []});
+    }
+
+    // ToDo: Parallelize vs IN query
+    // For each contract get all unpaid (paid=false) jobs
+    // const unpaidJobs = [];
+    // async.mapLimit(contractIds, 3, (contractId) => {
+    //     Job
+    // })
+
+    const unpaidJobs = await Job.findAll({
+      where: {
+        [Op.and]: [
+          {paid: false},
+          {ContractId: {[Op.in]: contractIds}}
+        ]
+      }
+    });
+    return res.json({unpaidJobs});
+  } catch (error) {
+    console.error(error);
+    return sendError(serverError(), res);
+  }
+
 });
 
 module.exports = app;
