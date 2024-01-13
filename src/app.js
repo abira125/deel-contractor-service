4b5f7b875116c58f4aadea0578a9add06f964c4d
abira125 <abhinavravi125@gmail.com>
2024-01-13 17:50:37 +0000

    Fix all linting problems

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
+const {sequelize} = require('./model');
+const {getProfile} = require('./middleware/getProfile');
 const app = express();
 app.use(bodyParser.json());
-app.set('sequelize', sequelize)
-app.set('models', sequelize.models)
+app.set('sequelize', sequelize);
+app.set('models', sequelize.models);
 
 const {getActiveContracts} = require('./services/ContractService');
 
@@ -13,38 +13,38 @@ const {getActiveContracts} = require('./services/ContractService');
  * @returns contract by id
  */
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
+  const {Contract} = req.app.get('models');
+  const {id} = req.params;
 
-    const {id: profileId, type: profileType} = req.profile;
+  const {id: profileId, type: profileType} = req.profile;
 
-    const contract = await Contract.findOne({where: {id}});
+  const contract = await Contract.findOne({where: {id}});
 
-    console.log('contract',contract);
+  console.log('contract',contract);
 
-    if (profileType === 'contractor' && profileId !== contract.ContractorId) {
-        return res.status(401).end();
-    }
+  if (profileType === 'contractor' && profileId !== contract.ContractorId) {
+    return res.status(401).end();
+  }
 
-    if (profileType === 'client' && profileId !== contract.ClientId) {
-        return res.status(401).end();
-    }
+  if (profileType === 'client' && profileId !== contract.ClientId) {
+    return res.status(401).end();
+  }
 
 
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
+  if(!contract) {return res.status(404).end();}
+  res.json(contract);
+});
 
 /**
  * @returns contracts
  */
 app.get('/contracts/', getProfile, async (req, res) => {
-    // Get all contracts for the given profile id
-    // ToDo: Don't pass req
-    const contracts = await getActiveContracts(req.profile, req);
+  // Get all contracts for the given profile id
+  // ToDo: Don't pass req
+  const contracts = await getActiveContracts(req.profile, req);
 
-    // Return response
-    res.json({ contracts});
-})
+  // Return response
+  res.json({ contracts});
+});
 
 module.exports = app;
